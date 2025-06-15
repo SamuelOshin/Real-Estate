9cd2b89644af6856cc8c76e5aed40bcc1e5105e3
SamuelOshin <samuelt.oshin@gmail.com>
2025-06-15 01:06:55 +0100

    Reset

--- venv/Real-EstateWeb/realtors/utils.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_random_password():
-    letters = ''.join(secrets.choice(string.ascii_letters) for i in range(8))
-    numbers = ''.join(secrets.choice(string.digits) for i in range(2))
-    password = letters + numbers
-    return password
